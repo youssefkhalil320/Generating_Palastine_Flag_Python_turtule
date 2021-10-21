9dd93ba5b5d6b3decedf45817e5696405bbe20a2
youssefkhalil320 <91757835+youssefkhalil320@users.noreply.github.com>
2021-10-21 13:23:16 +0200

    Add files via upload

---
+++ qr-gen.py
@@ -0,0 +1,72 @@
+import turtle
+from turtle import *
+
+# screen for output
+screen = turtle.Screen()
+
+# Defining a turtle Instance
+t = turtle.Turtle()
+speed(0)
+
+# initially penup()
+t.penup()
+t.goto(-400, 250)
+t.pendown()
+
+# Orange Rectangle
+# white rectangle
+import turtle
+from turtle import *
+
+# screen for output
+screen = turtle.Screen()
+
+# Defining a turtle Instance
+t = turtle.Turtle()
+speed(0)
+
+# initially penup()
+t.penup()
+t.goto(-400, 250)
+t.pendown()
+
+# Orange Rectangle
+# white rectangle
+t.color("black")
+t.begin_fill()
+t.forward(800)
+t.right(90)
+t.forward(167)
+t.right(90)
+t.forward(800)
+t.end_fill()
+t.left(90)
+t.forward(167)
+
+# Green Rectangle
+t.color("green")
+t.begin_fill()
+t.forward(167)
+t.left(90)
+t.forward(800)
+t.left(90)
+t.forward(167)
+t.end_fill()
+t.color("white")
+t.goto(-130, 81)
+t.color("black")
+t.goto(-400, 250)
+# white rectangle
+t.color("red")
+t.begin_fill()
+
+t.goto(0, 0)
+t.left(45)
+t.goto(-400, -250)
+
+t.end_fill()
+
+
+
+turtle.done()
+
